b4868d925b8c8a033d338b20bb0e7aef0e37f410
xuhao <1085330710@qq.com>
2020-11-14 13:28:27 +0800

    finish task1

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,10 +1,104 @@
-public class LinkedListDeque{
-    public class Node{
-        
+public class LinkedListDeque<T>{
+
+    private class Node {
+        public Node prev;
+        public Node next;
+        public T item;
+
+        public Node(T i){
+            item = i;
+            prev = null;
+            next = null;
+        }
+
     }
-    public void add();
-    public void remove();
-    public T get();
-    public int size();
+
+    public Node sentinel;
+    public int size;
+
+    public LinkedListDeque(){
+        sentinel = new Node(null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+    public void addFirst(T item){
+        Node newnode = new Node(item);
+        /** Newnode's prev should be the sentinel and it's next shou-
+         * ld be the node that usd to be the head
+         */
+        newnode.prev = sentinel;
+        newnode.next = sentinel.next;
+        /** Sentinel's next change to the new node and the old head's
+         * prev should be the newnode.
+         */
+        sentinel.next.prev = newnode;
+        sentinel.next = newnode;
+        /** size increase by one
+         */
+        size++;
+    }
+
+    public void addLast(T item){
+        Node newnode = new Node(item);
+        /** Newnode's prev should be the node that used to be the
+         * last which is sentienl.prev, and it's next should be the
+         * sentinel.
+         */
+        newnode.prev = sentinel.prev;
+        newnode.next = sentinel;
+        /** The old end of the Deque's next should be the new node
+         * and the sentinel.prev should be the newnode too.
+         */
+        sentinel.prev.next = newnode;
+        sentinel.prev = newnode;
+        /** size increase by one
+         */
+        size++;
+    }
+
+    public boolean isEmpty(){
+        boolean isempty = false;
+        if(size == 0){
+            isempty = true;
+        }
+        return isempty;
+    }
+
+    public void printDeque(){
+        Node pointer = sentinel;
+        while(!pointer.next.equals(sentinel)){
+            System.out.print(pointer.item + " ");
+            pointer = pointer.next;
+        }
+        System.out.println();
+    }
+
+    public void removeFirst(){
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size--;
+    }
+
+    public void removeLast(){
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+    }
+
+    public T get(int index){
+        int i = 0;
+        Node tempnode = sentinel;
+        while(i < index){
+            tempnode = tempnode.next;
+            i++;
+        }
+        return  tempnode.next.item;
+    }
+
+    public int size(){
+        return size;
+    }
 
 }
